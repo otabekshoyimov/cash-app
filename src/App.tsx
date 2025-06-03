1d76ceb3b87229226060e5afcaf772d44e2880ec
otabekshoyimov <otabek_s28@icloud.com>
2025-06-03 11:17:42 +0500

    Add some markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
 export function App() {
-
   return (
     <>
-    <div className="text-pink-400">testing...</div>
+      <section className="px-5 pt-5">
+        <Dashboard />
+      </section>
     </>
-  )
+  );
 }
+const Dashboard = () => {
+  return (
+    <>
+      <DashboardHeader />
+      <DashboardOverview />
+      <DashboardHistoy />
+    </>
+  );
+};
 
+const DashboardHeader = () => {
+  return (
+    <>
+      <section>
+        <header>dash header</header>
+        <main className="outline outline-1">dash header content</main>
+      </section>
+    </>
+  );
+};
+
+const DashboardOverview = () => {
+  return (
+    <>
+      <section>
+        <header>dash overview</header>
+        <main className="outline outline-1">dash overview content</main>
+      </section>
+    </>
+  );
+};
+
+const DashboardHistoy = () => {
+  return (
+    <section>
+      <header>dash history</header>
+      <main className="outline outline-1">dash history content</main>
+    </section>
+  );
+};
