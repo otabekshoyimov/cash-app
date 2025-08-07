ba5500f50f75bfce1aa947ff49cfe1cc8cec085b
otabekshoyimov <otabek_s28@icloud.com>
2025-08-07 16:50:36 +0500

    initial routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { useRef } from "react";
+import { useFetcher, useLoaderData } from "react-router";
+
+export async function indexLoader() {
+  const transactions = JSON.parse(localStorage.getItem("transactions") || "[]");
+  return transactions;
+}
+
+export async function indexAction({ request }) {
+  const formData = await request.formData();
+  const description = formData.get("description");
+  const amount = formData.get("amount");
+  console.log("Action received data:", { description, amount });
+
+  const existing_transanctions = JSON.parse(
+    localStorage.getItem("transactions") || "[]",
+  );
+  const new_transaction = { description: description, amount: Number(amount) };
+  const updated_transactions = [...existing_transanctions, new_transaction];
+  localStorage.setItem("transactions", JSON.stringify(updated_transactions));
+  return updated_transactions;
+}
+
 export function App() {
   return (
     <>
@@ -9,31 +32,85 @@ export function App() {
 }
 const Dashboard = () => {
   return (
-    <>
+    <section className="px-20 pt-20">
       <DashboardHeader />
       <DashboardOverview />
       <DashboardHistoy />
-    </>
+    </section>
   );
 };
 
 const DashboardHeader = () => {
+  const fetcher = useFetcher();
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
   return (
-    <>
-      <section>
-        <header>dash header</header>
-        <main className="outline outline-1">dash header content</main>
-      </section>
-    </>
+    <section>
+      <header>dash header</header>
+      <main className="outline outline-1 flex justify-between">
+        <span>dash header content</span>
+        <div className="flex gap-10">
+          <button onClick={() => dialogRef.current?.showModal()}>
+            new income
+          </button>
+          <dialog ref={dialogRef} className="pb-10">
+            <fetcher.Form className="flex flex-col" method="POST" action="/">
+              <section className="px-10 pt-10">
+                <header className="flex justify-between pb-10">
+                  <span>Create a new income transaction</span>
+                  <button onClick={() => dialogRef.current?.close()}>x</button>
+                </header>
+                <main className="pb-10 flex flex-col">
+                  <label htmlFor="">Description</label>
+                  <input type="text" name="description" />
+
+                  <label htmlFor="">Amount</label>
+                  <input type="number" name="amount" placeholder="0" />
+
+                  <label htmlFor="">Category</label>
+                  <select name="category" id="">
+                    <option value="">select an option</option>
+                    <option value="salary">salary</option>
+                    <option value="investments">investments</option>
+                  </select>
+
+                  <label htmlFor="">Transaction date</label>
+                  <input type="date" name="date" />
+                </main>
+              </section>
+              <footer className="flex justify-end pr-10 gap-10">
+                <button type="submit">
+                  {fetcher.state != "idle" ? "creating" : "create"}
+                </button>
+              </footer>
+            </fetcher.Form>
+          </dialog>
+          <button>new expense</button>
+        </div>
+      </main>
+    </section>
   );
 };
 
 const DashboardOverview = () => {
+  const transactions = useLoaderData();
+  console.log("transactions", transactions);
+
   return (
     <>
       <section>
         <header>dash overview</header>
-        <main className="outline outline-1">dash overview content</main>
+        <main className="outline outline-1">
+          <span>dash overview content</span>
+        </main>
+        <ul>
+          {transactions.map((transaction) => (
+            <li className="flex gap-10">
+              <span>{transaction.description}</span>
+              <span>{transaction.amount}</span>
+            </li>
+          ))}
+        </ul>
       </section>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import { App } from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { App, indexAction, indexLoader } from "./App.tsx";
+import "./index.css";
 
-
-const router =  createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/',
-    element: <App/>
-  }
-])
+    path: "/",
+    element: <App />,
+    action: indexAction,
+    loader: indexLoader,
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
- <RouterProvider router={router}/>
-)
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
+);
